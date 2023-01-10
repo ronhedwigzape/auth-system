c759bbc563ad32e0d9391286889c80c07aa52ace
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-11 00:10:09 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    require_once 'partials/header.php'
+?>
+<body>
+
+<!--    Main Content    -->
+<div class="container">
+<nav>
+    <div>
+
+    </div>
+</nav>
+    <h1>Hi</h1>
+</div>
+<div id="preloader"></div>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/preloader.js"></script>
+</body>
+</html>
