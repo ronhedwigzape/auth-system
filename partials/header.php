26a9dab3b7711768eeebf02a43b7b2df5706d101
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-11 00:10:19 +0800

    Create header.php

---
+++ partials/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="`en`">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--    Style-->
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <!--    Bootstrap    -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!--    jQuery      -->
+    <script src="js/jquery-v3-6-3-min.js"></script>
+    <title></title>
+</head>
