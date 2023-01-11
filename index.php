d7899d9f9ad512ffa663e75a195e2ecf1a541af7
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-11 17:13:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <?php
 
+    // Initialize the session
+    session_start();
+
     require_once 'partials/header.php'
 ?>
 <body>
-
+<div id="preloader"></div>
 <!--    Main Content    -->
 <div class="container">
-<nav>
-    <div>
 
+    <div class="d-flex justify-content-center" style="margin-top: 8rem; margin-bottom: 8rem;">
+        <h1 class="d-flex justify-content-center text-center tracking-in-expand" style="font-size: 6rem;">Hello World.</h1>
     </div>
-</nav>
-    <h1>Hi</h1>
+    <div class="row d-flex">
+        <div class="col-md-6 col-12 my-3 d-flex justify-content-center">
+            <button class="register-button"><a href="#register-component" style="color: #fff;text-decoration: none;">Click here to Register!</a></button>
+        </div>
+        <div class="col-md-6 col-12 my-3 d-flex justify-content-center">
+            <button class="login-button"><a href="login.php" style="color: #fff;text-decoration: none;">Click here to Login!</a></button>
+        </div>
+    </div>
+    <!--    Sign Up Component   -->
+    <div class="" style="margin-top: 45rem; margin-bottom: 20rem;" id="register-component">
+        <?php require_once 'partials/register.php' ?>
+    </div>
 </div>
-<div id="preloader"></div>
-<script src="assets/js/main.js"></script>
-<script src="assets/js/preloader.js"></script>
-</body>
-</html>
+<?php require_once 'partials/footer.php'; ?>
