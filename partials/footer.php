3e6c3b2875ea920709b36f07406022a092b1ebc3
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-15 22:49:12 +0800

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -14,26 +14,61 @@
             }
         }
 
-
         $("#toggle").change(function() {
             if ($(this).is(':checked')) {
                 // checkbox is checked
                 localStorage.setItem('darkMode', 'enabled');
                 $(".form-check-label").html('Switch to Light Mode');
-                $("body, input, h2, a, b, p, small, hr").addClass("dark-mode");
+                $("body, input").addClass("dark-mode");
+                $("h2, b, p, small, hr").addClass("text-white");
             } else {
                 // checkbox is not checked
                 localStorage.setItem('darkMode', 'disabled');
                 $(".form-check-label").html('Switch to Dark Mode');
-                $("body, input, h2, a, b, p, small, hr").removeClass("dark-mode");
+                $("body, input").removeClass("dark-mode");
+                $("h2, a, b, p, small, hr").removeClass("text-white");
             }
         });
 
         if (localStorage.getItem('darkMode') === 'enabled') {
-            $("body, input, h2, a, b, p, small, hr").addClass("dark-mode");
+            $("body, input").addClass("dark-mode");
+            $("h2, a, b, p, small, hr").addClass("text-white");
             $("#toggle").prop("checked", true);
         }
 
+
+        // if (getCookie("darkMode") === 'enabled') {
+        //     $("body, input, h2, a, b, p, small, hr").addClass("dark-mode");
+        //     $("#toggle").prop("checked", true);
+        // }
+        //
+        // $("#toggle").change(function() {
+        //     if (getCookie("darkMode") === 'enabled') {
+        //         setCookie("darkMode", "disabled", 365);
+        //         $("body, input, h2, a, b, p, small, hr").removeClass("dark-mode");
+        //     } else {
+        //         setCookie("darkMode", "enabled", 365);
+        //         $("body, input, h2, a, b, p, small, hr").addClass("dark-mode");
+        //     }
+        // });
+        //
+        // // function to get cookie value
+        // function getCookie(name) {
+        //     var value = "; " + document.cookie;
+        //     var parts = value.split("; " + name + "=");
+        //     if (parts.length == 2) return parts.pop().split(";").shift();
+        // }
+        //
+        // // function to set cookie
+        // function setCookie(name, value, days) {
+        //     var expires = "";
+        //     if (days) {
+        //         var date = new Date();
+        //         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        //         expires = "; expires=" + date.toUTCString();
+        //     }
+        //     document.cookie = name + "=" + (value || "") + expires + "; path=/";
+        // }
 
         // $(".signup").submit(function(event){
         //     event.preventDefault();
