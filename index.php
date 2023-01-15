069b72eb2b828578433127d1f72c6a99d760bd4c
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-15 22:49:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -126,7 +126,6 @@ require_once 'partials/header.php'
 <div id="preloader"></div>
 <!--    Main Content    -->
 <div class="container">
-
     <div class="d-flex justify-content-center mt-5" style=" margin-bottom:5rem;">
         <h1 class="d-flex justify-content-center text-center tracking-in-expand" style="font-size: 6rem;">Hello World.</h1>
     </div>
