5a0d61b814597c79665a9a88fa8fa42d6b700b07
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-15 22:07:52 +0800

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -13,6 +13,28 @@
                 p.attr("type", "password");
             }
         }
+
+
+        $("#toggle").change(function() {
+            if ($(this).is(':checked')) {
+                // checkbox is checked
+                localStorage.setItem('darkMode', 'enabled');
+                $(".form-check-label").html('Switch to Light Mode');
+                $("body, input, h2, a, b, p, small, hr").addClass("dark-mode");
+            } else {
+                // checkbox is not checked
+                localStorage.setItem('darkMode', 'disabled');
+                $(".form-check-label").html('Switch to Dark Mode');
+                $("body, input, h2, a, b, p, small, hr").removeClass("dark-mode");
+            }
+        });
+
+        if (localStorage.getItem('darkMode') === 'enabled') {
+            $("body, input, h2, a, b, p, small, hr").addClass("dark-mode");
+            $("#toggle").prop("checked", true);
+        }
+
+
         // $(".signup").submit(function(event){
         //     event.preventDefault();
         //
