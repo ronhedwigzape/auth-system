ab7b19535c4d068d067a4f6a62cc193f435ba271
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-15 14:21:56 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,9 +1,24 @@
 <?php
 // Initialize the session
 session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+
+//echo "<pre>";
+//echo var_dump($_COOKIE);
+//echo "</pre>";
+//
+//echo "<pre>";
+//echo var_dump($_SESSION);
+//echo "</pre>";
+
+
+// Check if the user is logged in, otherwise redirect to login page
+//if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+//    header("location: login.php");
+//    exit;
+//}
+
+// Check if the user is logged in, if not then redirect him to login page using cookies
+if(!isset($_COOKIE['username'])){
     header("location: login.php");
     exit;
 }
@@ -11,10 +26,12 @@ require_once 'partials/header.php';
 ?>
     <div id="preloader"></div>
     <div class="container">
-        <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+        <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_COOKIE["username"]); ?></b>. Welcome to our site.</h1>
         <p>
             <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-            <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+            <form action="login.php" method="POST">
+                <button type="submit" href="login.php" name="logout" class="btn btn-danger ml-3">Sign Out of Your Account</button>
+            </form>
         </p>
     </div>
 
