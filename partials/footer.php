d451d05e7b0e3f148e0166831509a9a5b552f271
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-16 12:38:00 +0800

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -2,6 +2,10 @@
 ?>
 <script>
     $(function (){
+        // Data Table JS
+        $('#myTable').DataTable();
+
+        // Toggle passsword visibility
         var button = $("form .toggle-eye");
         var p = $("#password, #confirm_password");
         button[0].onclick = function (e) {
@@ -16,13 +20,13 @@
 
         $("#toggle").change(function() {
             if ($(this).is(':checked')) {
-                // checkbox is checked
+                // Execute when checkbox is checked
                 localStorage.setItem('darkMode', 'enabled');
                 $(".form-check-label").html('Switch to Light Mode');
                 $("body, input").addClass("dark-mode");
                 $("h2, b, p, small, hr").addClass("text-white");
             } else {
-                // checkbox is not checked
+                // Execute when checkbox is not checked
                 localStorage.setItem('darkMode', 'disabled');
                 $(".form-check-label").html('Switch to Dark Mode');
                 $("body, input").removeClass("dark-mode");
@@ -37,38 +41,7 @@
         }
 
 
-        // if (getCookie("darkMode") === 'enabled') {
-        //     $("body, input, h2, a, b, p, small, hr").addClass("dark-mode");
-        //     $("#toggle").prop("checked", true);
-        // }
-        //
-        // $("#toggle").change(function() {
-        //     if (getCookie("darkMode") === 'enabled') {
-        //         setCookie("darkMode", "disabled", 365);
-        //         $("body, input, h2, a, b, p, small, hr").removeClass("dark-mode");
-        //     } else {
-        //         setCookie("darkMode", "enabled", 365);
-        //         $("body, input, h2, a, b, p, small, hr").addClass("dark-mode");
-        //     }
-        // });
-        //
-        // // function to get cookie value
-        // function getCookie(name) {
-        //     var value = "; " + document.cookie;
-        //     var parts = value.split("; " + name + "=");
-        //     if (parts.length == 2) return parts.pop().split(";").shift();
-        // }
-        //
-        // // function to set cookie
-        // function setCookie(name, value, days) {
-        //     var expires = "";
-        //     if (days) {
-        //         var date = new Date();
-        //         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        //         expires = "; expires=" + date.toUTCString();
-        //     }
-        //     document.cookie = name + "=" + (value || "") + expires + "; path=/";
-        // }
+
 
         // $(".signup").submit(function(event){
         //     event.preventDefault();
@@ -96,6 +69,8 @@
         // });
     });
 </script>
+
 <script src="assets/js/main.js"></script>
+
 </body>
 </html>
